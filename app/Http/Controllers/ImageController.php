ffdc5b14f7bb242d9efc1de914ad9ab0c9e420fd
ichsanputr <ngekode24@gmail.com>
2024-05-16 11:26:22 +0700

    up

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -15,7 +15,7 @@ class ImageController extends Controller
             $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->storeAs('public', $imageName); // You can change the storage path as needed
             return response()->json([
-                'data' => "http://127.0.0.1:8000" . "/storage/" . $imageName
+                'data' => env('APP_URL') . "/storage/" . $imageName
             ]);
         } else {
             return "No image uploaded!";
@@ -32,7 +32,7 @@ class ImageController extends Controller
         });
 
         $data = array_map(function ($val) {
-            return "http://127.0.0.1:8000/" . str_replace("public", "storage", $val) ;
+            return env('APP_URL') . '/' . str_replace("public", "storage", $val) ;
         }, $data);
 
         return response()->json($data);

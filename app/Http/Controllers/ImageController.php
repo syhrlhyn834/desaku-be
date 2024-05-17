eaec85f177b391079ffe80d322c94339eebd88e5
ichsanputr <ngekode24@gmail.com>
2024-05-17 12:30:00 +0700

    rm

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\DB;
 
 class ImageController extends Controller
 {

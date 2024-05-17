9f330ca25f5cf990fc82a4adf4343a8fc5a2819a
ichsanputr <ngekode24@gmail.com>
2024-05-17 18:03:23 +0700

    hello

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class AuthController extends Controller
 {
     public function login(Request $req)
     {
+        $key = 'desaku89ajs';
+        $payload = [];
+
         $isUserExist = DB::table('user')
             ->where("email", $req->input("email"))
             ->where("password", $req->input("password"))
             ->exists();
 
-        if ($isUserExist){
-            return "User found!";
+        $jwt = JWT::encode($payload, $key, 'HS256');
+
+        if ($isUserExist) {
+            return response()->json(['token' => $jwt]);
         } else {
             return response()->json(['error' => 'Unauthorized'], 403);
         }

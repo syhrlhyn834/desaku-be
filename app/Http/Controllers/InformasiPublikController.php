c7cb5a0df685371bf9e017c88ea94ed83edb0bf7
ichsanputr <ngekode24@gmail.com>
2024-05-24 18:42:13 +0700

    up

--- app/Http/Controllers/InformasiPublikController.php
+++ app/Http/Controllers/InformasiPublikController.php
@@ -49,6 +49,7 @@ class InformasiPublikController extends Controller
 
     public function getAnnouncement(Request $req)
     {
+        echo $req->query("limit");
         if ($req->query('limit')) {
             $data = DB::table('pengumuman')->limit($req->query('limit'))->get();
         } else {
